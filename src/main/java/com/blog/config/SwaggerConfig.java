c45049e6778cfb1dac97b4fcac48e4400bff79df
Nikita Bodke <nikitabodke262@gmail.com>
2022-06-16 20:56:02 +0530

    commit

--- src/main/java/com/blog/config/SwaggerConfig.java
+++ src/main/java/com/blog/config/SwaggerConfig.java
@@ -1,18 +1,28 @@
 package com.blog.config;
 
 import org.hibernate.mapping.Collection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
 import java.util.Collections;
-import io.swagger.models.Contact;
+import java.util.List;
+
+//import io.swagger.models.Contact;
+import springfox.documentation.service.Contact;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
+	@Bean
 	public Docket api() {
 	return new Docket(DocumentationType.SWAGGER_2).
 			apiInfo(getInfo()).select().apis(RequestHandlerSelectors.any())
@@ -21,12 +31,18 @@ public class SwaggerConfig {
 
 	private ApiInfo getInfo() {
 		
+		 Contact contact = new Contact( "Sergey Kargopolov",
+	               "https://www.appsdeveloperblog.com", 
+	               "developer@www.appsdeveloperblog.com" );
+	       
+	       List<VendorExtension> vendorExtensions = new ArrayList<>();
+		
 		// TODO Auto-generated method stub
 		return new ApiInfo("Blogging Application", 
 				"Developed to learn backend devlopment ",
 				"1.0", "Terms of Service",
-				new Contact(),
+				contact,
 				"License",
-				"API", Collections.emptyList() );
+				"API", vendorExtensions);
 	}
 }
